cd432c86e07038f6261102b9c707b19b493d59d1
adrian-pieroni <adrian.pieroni02@gamil.com>
2025-02-18 20:01:50 -0300

    actualiza limpieza de caja

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
 
 //Implementa una función para agregar amigos.
 
@@ -8,17 +8,12 @@
 
 //Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
 
-//Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
-
 //Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
 
 //Inicializo la lista de amigos como un array vacío.
 let listaAmigos = [];
 
-//inicializo la variable nuevoAmigo
-//let nuevoAmigo = ""
 
-//Pido el ingreso del nuevo amigo.
 //nuevoAmigo = prompt("Ingrese su nuevo amigo");
 let listaDeAmigos = [];
 
@@ -27,21 +22,31 @@ function agregarAmigo() {
     // Obtener el elemento del input por su ID
     const inputAmigo = document.getElementById('amigo').value;
 
-  
-    // Obtener el valor ingresado en el input
-
-  
     // Hacer algo con el valor obtenido (ejemplo: mostrar en consola)
     console.log(`El nombre ingresado es: ${inputAmigo}`);
     console.log(inputAmigo);
-    // Limpiar el input (opcional)
-   // inputAmigo.value = '';
 
-listaDeAmigos.push(inputAmigo);
-
-console.log(listaDeAmigos);
+   //Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push()
+    if (inputAmigo == ""){
+    alert("Debes ingresar un nombre válido");} else {
+    listaDeAmigos.push(inputAmigo);
+    console.log(listaDeAmigos);
+    limpiarCaja();
+    }
+    return listaAmigos;
 }
+ // Limpiar el input (opcional)
+
+ function limpiarCaja(){
+    let valorCaja = document.querySelector('#amigo');
+    valorCaja ='';
+ }
+
+ function limpiarCaja(){
+    let valorCaja = document.getElementById('amigo').value="";
+  
+ }
 
 //Generar un bucle para que se repita el agredao y gurdado de amigos en la lista.
 
-//Indicar la condición de salida del bucle.
+//Indicar la condición de salida del bucle
